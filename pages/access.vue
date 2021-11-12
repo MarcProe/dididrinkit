69792d9b7ae0a5c13641b6d28f339c64936eb213
Marcus Proest <marcus@proest.net>
2021-11-12 21:58:32 +0100

    fix: check if spinner works

--- pages/access.vue
+++ pages/access.vue
@@ -10,7 +10,11 @@
 export default {
   created() {
     this.$store.commit('set_token', this.$route.query.access)
+    sleep(3000)
     this.$router.push('/')
   },
+}
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 </script>
