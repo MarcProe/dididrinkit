3d9b235dc7025013d71211ef61f7904133891045
Marcus Proest <marcus@proest.net>
2021-11-12 22:02:09 +0100

    fix: works

--- pages/access.vue
+++ pages/access.vue
@@ -10,11 +10,7 @@
 export default {
   created() {
     this.$store.commit('set_token', this.$route.query.access)
-    sleep(3000)
     this.$router.push('/')
   },
-}
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 </script>
