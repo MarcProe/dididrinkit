82123cfb47eff46befc6bb4293c0b87b90864392
Marcus Proest <proemarc@gmail.com>
2022-09-19 21:46:51 +0200

    do not apply multi-word-component-names for pages

--- components/CreditsItem.vue
+++ components/CreditsItem.vue
@@ -1,12 +1,6 @@
 <template>
-  <b-alert
-    :show="dismissCountDown"
-    fade
-    variant="dark"
-    class="position-fixed fixed-bottom m-0 rounded-0"
-    style="z-index: 2000"
-    @dismiss-count-down="countDownChanged"
-  >
+  <b-alert :show="dismissCountDown" fade variant="dark" class="position-fixed fixed-bottom m-0 rounded-0"
+    style="z-index: 2000" @dismiss-count-down="countDownChanged">
     <a href="https://untappd.com" target="_new">
       <b-img src="@/static/pbu_40_black.png" fluid center />
     </a>
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,13 @@
   <div>
     <TopBar id="slider" />
     <BeerList />
-    <Credits />
+    <CreditsItem />
     <SideBar />
   </div>
 </template>
 
 <script>
+import CreditsItem from '~/components/CreditsItem.vue'
 function modalError(that) {
   const modalOptions = {
     title: 'Error ' + that.$store.state.error.code,
@@ -27,60 +28,63 @@ export default {
   data() {
     return {
       beers: [],
-    }
+    };
   },
-
   mounted() {
-    this.$store.commit('set_beers', JSON.parse(localStorage.getItem('beers')))
-    this.$store.commit('set_wishlist', JSON.parse(localStorage.getItem('wishlist')))
-    this.$store.commit('set_user', JSON.parse(localStorage.getItem('user')))
-    this.$store.commit('set_meta', JSON.parse(localStorage.getItem('meta')))
-    this.$store.commit('set_settings', JSON.parse(localStorage.getItem('settings')))
+    this.$store.commit("set_beers", JSON.parse(localStorage.getItem("beers")));
+    this.$store.commit("set_wishlist", JSON.parse(localStorage.getItem("wishlist")));
+    this.$store.commit("set_user", JSON.parse(localStorage.getItem("user")));
+    this.$store.commit("set_meta", JSON.parse(localStorage.getItem("meta")));
+    this.$store.commit("set_settings", JSON.parse(localStorage.getItem("settings")));
     // this.$store.commit('set_token', JSON.parse(localStorage.getItem('token')))
-
     if (!this.$store.state.user) {
       if (this.$store.state.access_token) {
-        this.$store.dispatch('getUserInfo').then((r) => {
+        this.$store.dispatch("getUserInfo").then((r) => {
           if (r.ok) {
-            localStorage.setItem('user', JSON.stringify(this.$store.state.user))
-          } else {
-            modalError(this)
+            localStorage.setItem("user", JSON.stringify(this.$store.state.user));
           }
-        })
+          else {
+            modalError(this);
+          }
+        });
       }
     }
-
     if (!this.$store.state.beers) {
-      this.$store.commit('set_beers', [])
+      this.$store.commit("set_beers", []);
       if (this.$store.state.access_token) {
-        this.$store.dispatch('getUserBeers').then((r) => {
+        this.$store.dispatch("getUserBeers").then((r) => {
           if (r.ok) {
-            localStorage.setItem('beerstimestamp', new Date().toLocaleString())
-            localStorage.setItem('beers', JSON.stringify(this.$store.state.beers))
-            this.$store.commit('set_bshown', this.$store.state.beers?.length)
-            localStorage.setItem('meta', JSON.stringify(this.$store.state.meta))
-          } else {
-            modalError(this)
+            localStorage.setItem("beerstimestamp", new Date().toLocaleString());
+            localStorage.setItem("beers", JSON.stringify(this.$store.state.beers));
+            this.$store.commit("set_bshown", this.$store.state.beers?.length);
+            localStorage.setItem("meta", JSON.stringify(this.$store.state.meta));
           }
-        })
+          else {
+            modalError(this);
+          }
+        });
       }
-    } else this.$store.commit('set_bshown', this.$store.state.beers?.length)
-
+    }
+    else
+      this.$store.commit("set_bshown", this.$store.state.beers?.length);
     if (!this.$store.state.wishlist) {
-      this.$store.commit('set_wishlist', [])
+      this.$store.commit("set_wishlist", []);
       if (this.$store.state.access_token) {
-        this.$store.dispatch('getUserWishlist').then((r) => {
+        this.$store.dispatch("getUserWishlist").then((r) => {
           if (r.ok) {
-            localStorage.setItem('wishlist', JSON.stringify(this.$store.state.wishlist))
-            this.$store.commit('set_wshown', this.$store.state.wishlist?.length)
-          } else {
-            modalError(this)
+            localStorage.setItem("wishlist", JSON.stringify(this.$store.state.wishlist));
+            this.$store.commit("set_wshown", this.$store.state.wishlist?.length);
           }
-        })
+          else {
+            modalError(this);
+          }
+        });
       }
-    } else this.$store.commit('set_wshown', this.$store.state.wishlist?.length)
-
-    this.$store.dispatch('filterList')
+    }
+    else
+      this.$store.commit("set_wshown", this.$store.state.wishlist?.length);
+    this.$store.dispatch("filterList");
   },
+  components: { CreditsItem }
 }
 </script>
