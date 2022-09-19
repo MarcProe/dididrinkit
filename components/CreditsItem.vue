717ecb51813802510fb354aa9b5f7460a48f0228
Marcus Proest <proemarc@gmail.com>
2022-09-19 22:35:12 +0200

    fix linter problems

--- components/CreditsItem.vue
+++ components/CreditsItem.vue
@@ -1,5 +1,6 @@
 <template>
-  <b-alert :show="dismissCountDown" fade variant="dark" class="position-fixed fixed-bottom m-0 rounded-0"
+  <b-alert 
+    :show="dismissCountDown" fade variant="dark" class="position-fixed fixed-bottom m-0 rounded-0"
     style="z-index: 2000" @dismiss-count-down="countDownChanged">
     <a href="https://untappd.com" target="_new">
       <b-img src="@/static/pbu_40_black.png" fluid center />
